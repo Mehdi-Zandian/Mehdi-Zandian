1764c054a8b963e9e1116cf84ac030c37907c4c7
MehdiZandian <mahdizandi81@gmail.com>
2022-01-05 19:06:39 +0330

    update nav UI

--- src/components/Navbar/Navbar.js
+++ src/components/Navbar/Navbar.js
@@ -11,10 +11,10 @@ function Navbar() {
         <img src={logo} className="nav__logo" alt="Logo" />
 
         <ul className="nav__items align-items-center m-0 d-none d-md-flex list-unstyled">
-          <li className="me-4 px-1">Home</li>
-          <li className="mx-4 px-1">About</li>
-          <li className="mx-4 px-1">Projects</li>
-          <li className="ms-4 me-5 px-1">Skills</li>
+          <li className="me-4 pe-1">Home</li>
+          <li className="mx-4 pe-1">About</li>
+          <li className="mx-4 pe-1">Projects</li>
+          <li className="ms-4 me-5 pe-1">Skills</li>
           <li className="px-lg-5 py-lg-3 px-md-3 py-md-2 nav__itemsActive">
             Contact Me
           </li>
