4d5d417f65e5c0a056d77d130d30d48c246c5c01
MehdiZandian <mahdizandi81@gmail.com>
2022-01-11 13:16:13 +0330

    Fix Bug(1) Slider

--- src/components/ProjectDetail/ProjectSlider.js
+++ src/components/ProjectDetail/ProjectSlider.js
@@ -10,8 +10,35 @@ function ProjectSlider({ data }) {
     centerMode: true,
     variableWidth: true,
     speed: 500,
-    slidesToShow: 1,
-    slidesToScroll: 1,
+    slidesToShow: 4,
+    slidesToScroll: 4,
+    initialSlide: 0,
+    responsive: [
+      {
+        breakpoint: 1024,
+        settings: {
+          slidesToShow: 3,
+          slidesToScroll: 3,
+          infinite: true,
+          dots: true,
+        },
+      },
+      {
+        breakpoint: 600,
+        settings: {
+          slidesToShow: 2,
+          slidesToScroll: 2,
+          initialSlide: 2,
+        },
+      },
+      {
+        breakpoint: 480,
+        settings: {
+          slidesToShow: 1,
+          slidesToScroll: 1,
+        },
+      },
+    ],
   };
 
   return (
@@ -38,9 +65,8 @@ function ProjectSlider({ data }) {
               <img
                 loading="lazy"
                 style={{
+                  width: "auto",
                   height: "100%",
-                  objectFit: "cover",
-                  objectPosition: "center",
                 }}
                 src={pic}
                 alt="Project Screenshot"
