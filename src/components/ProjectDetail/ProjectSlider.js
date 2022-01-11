940ec85e643b2dc4d07c7964d51422b38b646710
MehdiZandian <mahdizandi81@gmail.com>
2022-01-11 16:58:25 +0330

    Fix Bug(1) slider

--- src/components/ProjectDetail/ProjectSlider.js
+++ src/components/ProjectDetail/ProjectSlider.js
@@ -12,7 +12,7 @@ function ProjectSlider({ data }) {
     speed: 500,
     slidesToShow: 3,
     slidesToScroll: 3,
-    initialSlide: 1,
+    initialSlide: 3,
     responsive: [
       {
         breakpoint: 1024,
