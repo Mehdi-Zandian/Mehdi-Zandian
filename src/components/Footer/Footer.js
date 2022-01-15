c793f44996e3019cca58e60a90108821c6a17bc3
MehdiZandian <mahdizandi81@gmail.com>
2022-01-15 14:15:28 +0330

    finalize and fix bug ⚡

--- src/components/Footer/Footer.js
+++ src/components/Footer/Footer.js
@@ -1,7 +1,4 @@
-import React from "react";
 import { HashLink as Link } from "react-router-hash-link";
-// animation
-import Zoom from "react-reveal/Zoom";
 // UI
 import { BsInstagram, BsGithub } from "react-icons/bs";
 import { HiArrowCircleUp } from "react-icons/hi";
@@ -12,25 +9,30 @@ function Footer({ href }) {
   return (
     <div className="footer w-100">
       <div className="footer__body w-100 d-flex flex-column justify-content-center text-center">
-        <Zoom cascade>
-          <div className="footer__bodyLinks">
-            <a
-              title="My Github Account"
-              href="https://github.com/Mehdi-Zandian"
-            >
-              <BsGithub />
-            </a>
-            <a
-              title="My Instagram Account"
-              href="https://www.instagram.com/_mehdizandi/"
-            >
-              <BsInstagram />
-            </a>
-            <a title="My Telegram Account" href="https://t.me/MehdiZandi">
-              <FaTelegramPlane />
-            </a>
-          </div>
-        </Zoom>
+        <div className="footer__bodyLinks">
+          <a
+            target="_blank"
+            title="My Github Account"
+            href="https://github.com/Mehdi-Zandian"
+          >
+            <BsGithub />
+          </a>
+          <a
+            target="_blank"
+            title="My Instagram Account"
+            href="https://www.instagram.com/_mehdizandi/"
+          >
+            <BsInstagram />
+          </a>
+          <a
+            target="_blank"
+            title="My Telegram Account"
+            href="https://t.me/MehdiZandi"
+          >
+            <FaTelegramPlane />
+          </a>
+        </div>
+
         <small className="text-muted mb-5">{`© ${new Date().getFullYear()} Mahdi Zandian`}</small>
         <div className="mb-4">
           <Link
@@ -46,4 +48,4 @@ function Footer({ href }) {
   );
 }
 
-export default React.memo(Footer);
+export default Footer;
